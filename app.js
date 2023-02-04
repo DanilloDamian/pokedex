42b4a295fc743d579050021c158dee5bb11024e6
Danillo <danillo.damian@hotmail.com>
2023-02-04 08:56:39 -0300

    Integrated with the database

--- app.js
+++ app.js
@@ -4,49 +4,58 @@ document.addEventListener("DOMContentLoaded", function(){
     
     const mainDiv = document.createElement('div');
     doc.appendChild(mainDiv);
-    mainDiv.classList.add('divCards');
+    mainDiv.classList.add('divCards'); 
+     
+    const addPokemons = (pokemons) =>{
+        pokemons.forEach(pokemon => {
+
+            const cardPokemon = document.createElement('div');
+            mainDiv.appendChild(cardPokemon);
+            cardPokemon.classList.add("cardPokemon");
+        
+            const cardHeader = document.createElement('div');
+            cardPokemon.appendChild(cardHeader);
+            cardHeader.classList.add('cardHeader');
+        
+            //button sound
+            const buttonSound = document.createElement('button');
+            buttonSound.type = 'button';
+            buttonSound.classList.add('buttonSound');
+            cardHeader.appendChild(buttonSound);        
+            
+            const imgSound = document.createElement('img')
+            buttonSound.appendChild(imgSound);
+            imgSound.src = 'img/sound.png';
+            imgSound.classList.add('imgSound');
+            //end button sound
+
+            const name = document.createElement('h2');
+            cardHeader.appendChild(name);
+            name.classList.add('nomePokemon');
+            name.textContent= pokemon.name;
+        
+            const internalDivisionCard = document.createElement('article');
+            cardPokemon.appendChild(internalDivisionCard);
+            internalDivisionCard.classList.add('articleCard');
+            
+            const img = document.createElement('img');
+            internalDivisionCard.appendChild(img);
+            img.classList.add('imgPokemon');
+            img.src = pokemon.img;
+        
+            const divStats = document.createElement('section');
+            internalDivisionCard.appendChild(divStats);
+            divStats.classList.add('sectionStats');
+
+        });
+    } 
+
+    fetch('https://raw.githubusercontent.com/alluzera/allupokedex/pokedex-API/pokestats.json?pageSize=20')
+    .then(response=>response.json())
+    .then(jsonData => addPokemons(jsonData.data));
+   
     
-    const pokemons = []; 
-
-    function createCard(pokemons){
-        const cardPokemon = document.createElement('div');
-        mainDiv.appendChild(cardPokemon);
-        cardPokemon.classList.add("cardPokemon");
-
-        const cardHeader = document.createElement('div');
-        cardPokemon.appendChild(cardHeader);
-        cardHeader.classList.add('cardHeader');
-
-        const buttonSound = document.createElement('button');
-        buttonSound.type = 'button';
-        buttonSound.classList.add('buttonSound');
-        cardHeader.appendChild(buttonSound);
-        
-        const imgSound = document.createElement('img')
-        buttonSound.appendChild(imgSound);
-        imgSound.src = 'img/sound.png';
-        imgSound.classList.add('imgSound');
-
-        const name = document.createElement('h2');
-        cardHeader.appendChild(name);
-        name.classList.add('nomePokemon');
-        name.textContent= pokemons.name;
-
-        const internalDivisionCard = document.createElement('article');
-        cardPokemon.appendChild(internalDivisionCard);
-        internalDivisionCard.classList.add('articleCard');
-        
-        const img = document.createElement('img');
-        internalDivisionCard.appendChild(img);
-        img.classList.add('imgPokemon');
-        img.src = pokemons.img;
-
-        const divStats = document.createElement('section');
-        internalDivisionCard.appendChild(divStats);
-        divStats.classList.add('sectionStats');
-    }
-    pokemons.forEach(element=> createCard(element));
-
-
+    
+   
 })
 
