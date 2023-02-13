062c679c1ed03db313d45e5bd634889d870d3a97
DanilloDamian <109249738+DanilloDamian@users.noreply.github.com>
2023-02-13 15:38:46 -0300

    Fixed name filter problem

--- README.md
+++ README.md
@@ -1,3 +1,24 @@
 <strong>This is a simple Pokedex's project in JavaScript.</strong>
 
 https://danillodamian.github.io/pokedex/
+
+
+The pokemon list is from an API:
+
+<img height="360em" src="https://github.com/DanilloDamian/pokedex/blob/main/img/Screenshot_1.png"/>
+
+Clicking on the card displays the statistics of the pokemon:
+
+<img height="360em" src="https://github.com/DanilloDamian/pokedex/blob/main/img/Screenshot_2.png"/>
+
+Entering characters in the search returns names that contain the typed letters:
+
+<img height="360em" src="https://github.com/DanilloDamian/pokedex/blob/main/img/Screenshot_3.png"/>
+
+Enables listing by pokemon type:
+
+<img height="360em" src="https://github.com/DanilloDamian/pokedex/blob/main/img/Screenshot_4.png"/>
+
+
+
+
--- app.js
+++ app.js
@@ -1,59 +1,59 @@
-document.addEventListener("DOMContentLoaded", function(){
+document.addEventListener("DOMContentLoaded", function () {
     const doc = document.querySelector('main');
-    
+
     const mainDiv = document.createElement('div');
     doc.appendChild(mainDiv);
-    mainDiv.classList.add('divCards'); 
+    mainDiv.classList.add('divCards');
 
-    const addPokemons = (pokemons) =>{
+    const addPokemons = (pokemons) => {
         pokemons.forEach(pokemon => {
-            
+
             const cardPokemon = document.createElement('div');
             mainDiv.appendChild(cardPokemon);
             cardPokemon.classList.add("cardPokemon");
-            cardPokemon.addEventListener('click',showStatus);
+            cardPokemon.addEventListener('click', showStatus);
 
             const cardHeader = document.createElement('div');
             cardPokemon.appendChild(cardHeader);
             cardHeader.classList.add('cardHeader');
-        
-            const blueBall = document.createElement('div');           
+
+            const blueBall = document.createElement('div');
             blueBall.classList.add('blueBall');
-            cardHeader.appendChild(blueBall);        
-            
+            cardHeader.appendChild(blueBall);
+
             const name = document.createElement('h2');
             cardHeader.appendChild(name);
             name.classList.add('nomePokemon');
-            name.textContent= pokemon.name;
-        
+            name.textContent = pokemon.name;
+
             const internalDivisionCard = document.createElement('article');
             cardPokemon.appendChild(internalDivisionCard);
             internalDivisionCard.classList.add('articleCard');
-                        
+
             const img = document.createElement('img');
             internalDivisionCard.appendChild(img);
             img.classList.add('imgPokemon');
             img.src = pokemon.img;
-            
+
             const listStats = document.createElement('div');
             internalDivisionCard.appendChild(listStats);
-            listStats.classList.add('divStats','invisible');
-            const arrayStats = Object.keys(pokemon.stats);            
-            
-            arrayStats.forEach((element)=>{                
-                if(element != 'total'){                    
+            listStats.classList.add('divStats', 'invisible');
+            const arrayStats = Object.keys(pokemon.stats);
+
+            arrayStats.forEach((element) => {
+                if (element != 'total') {
                     const stats = document.createElement('p');
                     listStats.appendChild(stats);
-                    stats.classList.add('nameListStats'); 
-                    stats.textContent = element.toUpperCase();                    
-                    const textStats = (pokemon.stats[element]/2);
+                    stats.classList.add('nameListStats');
+                    stats.textContent = element.toUpperCase();
+                    const textStats = (pokemon.stats[element] / 2);
                     const numStats = document.createElement('div');
                     listStats.appendChild(numStats);
                     numStats.style = `width:${textStats}%`;
                     numStats.classList.add('backGroundStats');
-                };            
+                };
             });
-        
+
             const divStats = document.createElement('section');
             internalDivisionCard.appendChild(divStats);
             divStats.classList.add('sectionStats');
@@ -62,72 +62,89 @@ document.addEventListener("DOMContentLoaded", function(){
             divStats.appendChild(buttonStatus);
             buttonStatus.classList.add('buttonStatus');
             buttonStatus.textContent = 'stats';
-           
+
             const pokemonTypes = document.createElement('div');
             divStats.appendChild(pokemonTypes);
-            pokemonTypes.classList.add('divTypes');                  
-            
+            pokemonTypes.classList.add('divTypes');
+
             for (let index = 0; index < pokemon.type.length; index++) {
                 const type = document.createElement('h3');
                 pokemonTypes.appendChild(type);
-                type.textContent= `${pokemon.type[index]}`;
-                type.classList.add('type',`type-${pokemon.type[index]}`);                                          
-            };  
-            
+                type.textContent = `${pokemon.type[index]}`;
+                type.classList.add('type', `type-${pokemon.type[index]}`);
+            };
+
             const imgButtons = document.createElement('img');
             cardPokemon.appendChild(imgButtons);
             imgButtons.classList.add('imgButtons');
-            imgButtons.src= 'img/button.png';
+            imgButtons.src = 'img/button.png';
         });
     };
-    
-    function showStatus(evt){
-        evt.preventDefault(); 
-        const forClick = this;   
+
+    function showStatus(evt) {
+        evt.preventDefault();
+        const forClick = this;
         forClick.childNodes[1].childNodes[0].classList.toggle('invisible');
         forClick.childNodes[1].childNodes[1].classList.toggle('invisible');
     };
 
     const filter = document.querySelector("#filter");
-    filter.addEventListener("input", function(){
-        const pokemons = document.querySelectorAll(".cardPokemon");
-        if(this.value.length>0){
-            for (let index = 0; index < pokemons.length; index++) {
-                const pokemon = pokemons[index];
-                const pokemonName = pokemon.querySelector(".nomePokemon").textContent;
-                const exp = new RegExp(this.value, "i");
-                exp.test(pokemonName) ? pokemon.classList.remove("invisible") : pokemon.classList.add("invisible");                               
-            }
-        }else{
-            for (let index = 0; index < pokemons.length; index++) {
-                const pokemon = pokemons[index];
-                pokemon.classList.remove('invisible');
-            }
-        }
-    });
-
     const typeSelect = document.querySelector("#select_type");
-    typeSelect.addEventListener("input",function(){
-        const pokemons = document.querySelectorAll(".cardPokemon");
-        if(this.value.length>0){
-            for (let index = 0; index < pokemons.length; index++) {
-                const pokemon = pokemons[index];
-                const listTypes = pokemon.querySelectorAll(".type");
-                let listTypesString = [];
-                listTypes.forEach(element=> listTypesString.push(element.textContent));
-                listTypesString.includes(this.value) ? pokemon.classList.remove("invisible") : pokemon.classList.add("invisible");
-            }
-        }else{
-            for (let index = 0; index < pokemons.length; index++) {
-                const pokemon = pokemons[index];
-                pokemon.classList.remove('invisible');
-            }
+
+    filter.addEventListener("input", function () {
+        removePokemons();
+        if (filter.value.length == 0) {
+            findByType(typeSelect.value);
+        } else {
+            findByName(filter.value, typeSelect.value);
         }
     });
 
-    fetch('https://raw.githubusercontent.com/alluzera/allupokedex/pokedex-API/pokestats.json?pageSize=20')
-    .then(response=>response.json())
-    .then(jsonData => addPokemons(jsonData.data))
-    .catch(e=>alert(e));     
+    function removePokemons() {
+        mainDiv.innerText = "";
+    }
+    async function findByType(type) {
+        const findPokemonByType = await fetch(`http://localhost:3000/type/${type}`)
+            .then(response => response.json())
+            .then(r => { addPokemons(r) })
+            .catch(e => { console.log(e); window.location.reload() });
+    }
+    async function findByName(name, type) {
+        if (type == "") {
+            const findPokemonByName = await fetch(`http://localhost:3000/name/${name}`)
+                .then(response => response.json())
+                .then(r => addPokemons(r))
+                .catch(e => console.log(e));
+        } else {
+            const findPokemonByType = await fetch(`http://localhost:3000/type/${type}`)
+                .then(response => response.json())
+                .then(r => {
+                    let list = [];
+                    r.forEach(element => {
+                        if ((element.name).toUpperCase().includes(name.toUpperCase())) {
+                            list.push(element)
+                        }
+                    });
+                    addPokemons(list);
+                }).catch(e => { console.log(e); window.location.reload() });
+        }
+    }
+
+    typeSelect.addEventListener("input", function () {
+        removePokemons();
+        if (typeSelect.value == '') {
+            findAll()
+        } else {
+            findByType(typeSelect.value)
+        }
+    });
+
+    async function findAll() {
+        fetch('http://localhost:3000')
+            .then(response => response.json())
+            .then(r => addPokemons(r))
+            .catch(e => alert(e));
+    }
+    findAll();
 });
 
